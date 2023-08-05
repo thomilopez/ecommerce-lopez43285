094d39dcaebd8d0e2fb16cef0d82132c50fcd36e
Thomas Lopez <thomasignaciolopez@gmail.com>
2023-08-05 05:23:18 -0300

    hasta clase 13

--- src/components/pages/itemlist/ItemListConteiner.jsx
+++ src/components/pages/itemlist/ItemListConteiner.jsx
@@ -1,30 +1,50 @@
 import { useParams } from "react-router-dom";
-import { products } from "../../productsMock";
+
 import ItemList from "./ItemList";
 import { useState , useEffect } from "react";
-
+import { db } from "../../../firebaseConfig";
+import { getDocs, collection, query, where } from "firebase/firestore"
 
 const ItemListConteiner = () => {
 
     const [items, setItems] = useState ([]);
-    const [error, setError] = useState ({});
+   
 
     const {categoriasNombre } = useParams ();
 
     useEffect (() => {
-        let productosFiltrados = products.filter(
-            (elemento) => elemento.category === categoriasNombre
-        );
-        const buscar = new Promise((resolve, reject)=> {
-            resolve(categoriasNombre === undefined ? products : productosFiltrados);
-            reject ({message: "ERROR", status: 401})
-    });
-    
-    buscar.then((resp) => setItems(resp))
-            .catch((err) => setError(err));
+        let productsCollection = collection(db, "productos")
+        let consulta;
+        if(categoriasNombre){
+            consulta = query (productsCollection, where("category", "==", categoriasNombre))
+        }else{
+            consulta = productsCollection
+        }
+        
+        getDocs(consulta).then((elemento)=>{
+            let productos = elemento.docs.map( doc =>{
+                return {...doc.data(), id: doc.id}
+            })
+            setItems(productos)
+        })
+        
+        
+
     }, [categoriasNombre]);
+
+
+
+
+
+
 
     return <ItemList items={items} />;
 };
 
 export default ItemListConteiner
+
+
+
+
+
+
